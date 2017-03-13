2eb0b173e4f13a0d09681d342dddd0717b96fd3b
Martijn Harmenzon <martijn.harmenzon@maptm.nl>
2017-03-13 10:23:25 +0100

    Version 0.1.1
    Changed the sleep time to match the timedelta to the next half hour.

--- tracker.py
+++ tracker.py
@@ -1,9 +1,16 @@
 import os
 import time
+from datetime import datetime, timedelta
 
 while True:
-    print("Start: %s" % time.ctime())
     os.system("sudo python3 /services/python/flights/getGF.py 1")
     os.system("sudo python3 /services/python/flights/analyseGF.py 1")
-    print("End: %s" % time.ctime())
-    time.sleep(1800)
+
+    now = datetime.now()
+    next = now + timedelta(minutes=30)
+    next -= timedelta(minutes=next.minute % 30,
+                      seconds=next.second,
+                      microseconds=next.microsecond)
+    pause = (next - now).total_seconds()
+
+    time.sleep(pause)
